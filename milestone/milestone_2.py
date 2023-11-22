30cd8f2cc537a93ee25ad42410cacd8fab01b611
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-11-22 21:21:32 +0000

    Refactoring code

--- milestone/milestone_2.py
+++ milestone/milestone_2.py
@@ -1,11 +1,25 @@
 import random
 
-word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry']
-word = random.choice(word_list)
+def select_random_word(word_list):
+    return random.choice(word_list)
 
-print("Enter a letter: ")
-guess = input()
-if len(guess) == 1 and guess.isalpha() == True:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+def is_valid_guess(guess):
+    return len(guess) == 1 and guess.isalpha()
+
+def main():
+    word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry']
+    word = select_random_word(word_list)
+
+    print("Enter a letter: ")
+    guess = input().lower()  
+
+    if is_valid_guess(guess):
+        if guess in word:
+            print("Good guess!")
+        else:
+            print("Oops! That letter is not in the word.")
+    else:
+        print("Oops! That is not a valid input.")
+
+if __name__ == "__main__":
+    main()
