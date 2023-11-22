b98cfdf4f5a4b8f128cecd5295de12d3bb0efaf7
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-11-22 20:06:05 +0000

    check that the input is a single char

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -5,4 +5,7 @@ word = random.choice(word_list)
 
 print("Enter a letter: ")
 guess = input()
-print(guess)
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
