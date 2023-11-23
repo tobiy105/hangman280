0f1c9867e63592fd8552e51a455a09effab65646
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-11-23 17:49:33 +0000

    refactoring

--- milestone/milestone_3.py
+++ milestone/milestone_3.py
@@ -1,31 +1,31 @@
-def ask_for_input():
+def get_user_guess():
     print("Guess a letter: ")
     guess = input()
     return guess
 
-def check_guess(word, guess):
+def is_guess_correct(word, guess):
     guess = guess.lower()
-    if guess in word:
-        return True
-    else:
-        return False
+    return guess in word
 
-def is_valid_guess(guess):
+def is_valid_letter_guess(guess):
     return len(guess) == 1 and guess.isalpha()
+
+def print_message(message):
+    print(message)
 
 def main():
     secret_word = "apple"
     while True:
-        guess = ask_for_input()
+        guess = get_user_guess()
 
-        if is_valid_guess(guess):
-            if check_guess(secret_word, guess):
-                print(f"Good guess! {guess} is in the word.")
+        if is_valid_letter_guess(guess):
+            if is_guess_correct(secret_word, guess):
+                print_message(f"Good guess! {guess} is in the word.")
                 break
             else:
-                print(f"Sorry, {guess} is not in the word. Try again.")
+                print_message(f"Sorry, {guess} is not in the word. Try again.")
         else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
+            print_message("Invalid letter. Please, enter a single alphabetical character.")
 
 if __name__ == "__main__":
     main()
