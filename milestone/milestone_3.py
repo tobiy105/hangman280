01bd37d91b6e94d205fcc6896df6e36db1722f70
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-11-23 16:39:24 +0000

    Created checks

--- milestone/milestone_3.py
+++ milestone/milestone_3.py
@@ -1,3 +1,14 @@
+def ask_for_input():
+    print("Guess a letter: ")
+    guess = input()
+    return guess
+
+def check_guess(word, guess):
+    guess = guess.lower()
+    if guess in word:
+        return True
+    else:
+        return False
 
 def is_valid_guess(guess):
     return len(guess) == 1 and guess.isalpha()
@@ -5,18 +16,16 @@ def is_valid_guess(guess):
 def main():
     secret_word = "apple"
     while True:
-        print("Guess a letter: ")
-        guess = input()
+        guess = ask_for_input()
 
         if is_valid_guess(guess):
-            if guess in secret_word:
+            if check_guess(secret_word, guess):
                 print(f"Good guess! {guess} is in the word.")
                 break
             else:
                 print(f"Sorry, {guess} is not in the word. Try again.")
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
-
 
 if __name__ == "__main__":
     main()
