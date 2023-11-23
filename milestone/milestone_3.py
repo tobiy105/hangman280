86b354aa57d682b40665d98ee44c0b39ddd3e169
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2023-11-23 16:11:53 +0000

    check wheter the guess is in the word

--- milestone/milestone_3.py
+++ milestone/milestone_3.py
@@ -3,13 +3,17 @@ def is_valid_guess(guess):
     return len(guess) == 1 and guess.isalpha()
 
 def main():
+    secret_word = "apple"
     while True:
         print("Guess a letter: ")
         guess = input()
 
         if is_valid_guess(guess):
-            print("Good guess!") 
-            break
+            if guess in secret_word:
+                print(f"Good guess! {guess} is in the word.")
+                break
+            else:
+                print(f"Sorry, {guess} is not in the word. Try again.")
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
 
